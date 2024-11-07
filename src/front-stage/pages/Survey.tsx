968486a7168d6c8f211f2a526b2052601124f040
Xolodog <losididog@gmail.com>
2024-11-07 16:14:31 +0800

    feat:動態生成問卷功能

--- src/front-stage/pages/Survey.tsx
+++ src/front-stage/pages/Survey.tsx
@@ -1,5 +1,18 @@
-import { Box, Button, FormControl, TextField, Typography, Paper, styled } from '@mui/material';
-import { FormEvent, useState } from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  TextField,
+  Typography,
+  Paper,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormGroup,
+  Checkbox,
+  styled,
+} from '@mui/material';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { survey } from './fakedata';
 
 // 定義獨立的樣式元件
@@ -32,22 +45,41 @@ function LabeledTextField({
   );
 }
 
-function Answer() {
-  const [formData, setFormData] = useState({
+function Survey() {
+  const [answers, setAnswers] = useState<any>({
     name: '',
     phone: '',
     email: '',
     age: '',
   });
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { id, value } = event.target;
-    setFormData({ ...formData, [id]: value });
+    setAnswers({ ...answers, [id]: value });
+    console.log(answers);
+  }
+
+  function handleRadioChange(event: ChangeEvent<HTMLInputElement>, questionId: string) {
+    const { value } = event.target;
+    console.log(value);
+    setAnswers({ ...answers, [questionId]: value });
+  }
+
+  function handleCheckboxChange(event: ChangeEvent<HTMLInputElement>, questionId: string) {
+    const { value, checked } = event.target;
+    setAnswers({
+      ...answers,
+      [questionId]: {
+        ...answers[questionId],
+        [value]: checked,
+      },
+    });
+    console.log(answers);
   }
 
   function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(formData);
+    console.log(answers);
   }
 
   return (
@@ -64,32 +96,88 @@ function Answer() {
         <LabeledTextField
           label="姓名"
           id="name"
-          value={formData.name}
+          value={answers.name}
           onChange={handleInputChange}
         />
         <LabeledTextField
           label="手機"
           id="phone"
           type="tel"
-          value={formData.phone}
+          value={answers.phone}
           onChange={handleInputChange}
         />
         <LabeledTextField
           label="Email"
           id="email"
           type="email"
-          value={formData.email}
+          value={answers.email}
           onChange={handleInputChange}
         />
         <LabeledTextField
           label="年齡"
           id="age"
           type="number"
-          value={formData.age}
+          value={answers.age}
           onChange={handleInputChange}
         />
 
-        <Box textAlign="right">
+        {/* 動態生成問卷 */}
+        {survey.questions.map((question) => (
+          <Box key={question.id}>
+            <Typography variant="h6" sx={{ mt: 3 }}>
+              {question.questionText}
+            </Typography>
+            {question.type === 'single-choice' && (
+              <FormControl component="fieldset" fullWidth sx={{ mb: 2 }}>
+                <RadioGroup
+                  name={String(question.id)}
+                  value={answers[question.id] || ''}
+                  onChange={(event) => handleRadioChange(event, String(question.id))}
+                >
+                  {question.options?.map((option: any) => (
+                    <FormControlLabel
+                      key={option.optionId}
+                      control={<Radio />}
+                      label={option.optionText}
+                      value={option.optionId}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+            {question.type === 'multiple-choice' && (
+              <FormControl component="fieldset" fullWidth sx={{ mb: 2 }}>
+                <FormGroup>
+                  {question.options?.map((option: any) => (
+                    <FormControlLabel
+                      key={option.optionId}
+                      control={<Checkbox />}
+                      label={option.optionText}
+                      value={option.optionId || ''}
+                      checked={answers[question.id]?.[option.optionId] || false}
+                      onChange={(event) => handleCheckboxChange(event, question.id)}
+                    />
+                  ))}
+                </FormGroup>
+              </FormControl>
+            )}
+            {question.type === 'text' && (
+              <StyledFormControl fullWidth>
+                <TextField
+                  id={String(question.id)}
+                  value={answers[question.id] || ''}
+                  onChange={(event) => handleInputChange(event)}
+                  required
+                  size="small"
+                  multiline
+                  rows={4}
+                />
+              </StyledFormControl>
+            )}
+          </Box>
+        ))}
+
+        <Box textAlign="right" sx={{ mt: 3 }}>
           <Button type="submit" variant="contained" color="secondary">
             送出
           </Button>
@@ -99,4 +187,4 @@ function Answer() {
   );
 }
 
-export default Answer;
+export default Survey;
